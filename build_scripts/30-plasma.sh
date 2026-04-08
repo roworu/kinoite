1542c826ade4df932045f1607df933395d23923d
roworu <mamoruessu@gmail.com>
2026-04-08 15:15:48 +0300

    + fix firefox installation + do not override wallpapers for now..

--- build_scripts/30-plasma.sh
+++ build_scripts/30-plasma.sh
@@ -6,7 +6,7 @@ set -ouex pipefail
 
 # swap system firefox to flatpak version
 dnf5 -y remove firefox firefox-langpacks
-flatpak install -y flathub org.mozilla.firefox
+flatpak install --noninteractive --system flathub org.mozilla.firefox
 
 # remove update tray icon
 rm /etc/xdg/autostart/org.kde.discover.notifier.desktop || true
@@ -18,6 +18,6 @@ rm -vf /usr/share/applications/htop.desktop || true
 rm -vf /usr/share/applications/nvtop.desktop || true
 
 # wallpapers
-ln -sf /usr/share/wallpapers/kw-wallpaper.jxl /usr/share/backgrounds/default.jxl
-ln -sf /usr/share/wallpapers/kw-wallpaper-darker.jxl /usr/share/backgrounds/default-dark.jxl
-rm -f /usr/share/backgrounds/default.xml
+# ln -sf /usr/share/wallpapers/kw-wallpaper.jxl /usr/share/backgrounds/default.jxl
+# ln -sf /usr/share/wallpapers/kw-wallpaper-darker.jxl /usr/share/backgrounds/default-dark.jxl
+# rm -f /usr/share/backgrounds/default.xml
