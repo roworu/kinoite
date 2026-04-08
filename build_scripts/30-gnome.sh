844164173034df2508e77648381a34fd63d4669c
roworu <mamoruessu@gmail.com>
2026-04-08 15:30:50 +0300

    + prettify gnome .desktop files deletion too

--- build_scripts/30-gnome.sh
+++ build_scripts/30-gnome.sh
@@ -3,5 +3,10 @@ echo "::group:: ===$(basename "$0")==="
 
 set -ouex pipefail
 
-rm -vf /usr/share/applications/htop.desktop || true
-rm -vf /usr/share/applications/nvtop.desktop || true
+# swap system firefox to flatpak version
+dnf5 -y remove firefox firefox-langpacks
+flatpak install --noninteractive --system flathub org.mozilla.firefox
+
+# remove some apps from start menu
+rm -vf /usr/share/applications/htop.desktop
+rm -vf /usr/share/applications/nvtop.desktop
