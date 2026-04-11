9ab436533d8367ce15fec621c1dc989c4a9fb0f4
roworu <mamoruessu@gmail.com>
2026-04-11 17:44:28 +0300

    + rewrite nvidia install again..

--- build_scripts/00-base.sh
+++ build_scripts/00-base.sh
@@ -41,3 +41,15 @@ packages=(
 )
 dnf5 -y install "${packages[@]}"
 dnf5 versionlock add "${packages[@]}"
+
+dnf5 -y remove firefox firefox-langpacks \
+    plasma-welcome plasma-drkonqi plasma-welcome-fedora plasma-discover-kns kcharselect
+
+# remove update tray icon
+rm -vf /etc/xdg/autostart/org.kde.discover.notifier.desktop
+
+# remove some apps from start menu
+rm -vf /usr/share/applications/org.kde.kdebugsettings.desktop
+rm -vf /usr/share/applications/org.kde.khelpcenter.desktop
+rm -vf /usr/share/applications/htop.desktop
+rm -vf /usr/share/applications/nvtop.desktop
