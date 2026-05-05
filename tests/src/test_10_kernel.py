07c27f48cf74ebaabb2a28ce390380a69f564d02
roworu <mamoruessu@gmail.com>
2026-05-05 10:34:45 +0300

    + fix kernel test

--- tests/src/test_10_kernel.py
+++ tests/src/test_10_kernel.py
@@ -4,5 +4,5 @@ def test_cachy_kernel(ssh_command):
     assert result.returncode == 0, \
         f"Command returned bad returncode: {result.returncode}. Full response: {result}"
 
-    assert result.stdout.strip() == "cachy", \
+    assert ("cachy" in result.stdout.strip()), \
         f"Cachy kernel not used. Full response {result.stdout}"
