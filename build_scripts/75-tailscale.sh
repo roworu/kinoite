9ca38a2cd7d4844bf8e9a6ee7dfa2f76c8b68520
roworu <mamoruessu@gmail.com>
2026-09-03 10:20:05 +0300

    verbose deletion; fix formatting

--- build_scripts/75-tailscale.sh
+++ build_scripts/75-tailscale.sh
@@ -2,13 +2,13 @@
 
 set -ouex pipefail
 
-# install tailscale, keep disabled; 
+# install tailscale, keep disabled;
 # user enables with
 # systemctl enable --now tailscaled
 
 dnf5 -y config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 dnf5 -y install tailscale
-rm -f /etc/yum.repos.d/tailscale.repo
+rm -fv /etc/yum.repos.d/tailscale.repo
 systemctl disable tailscaled.service
 
 # disable extra logging
