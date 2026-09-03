c8b2b1cd0c26d10af6c694f99b1a8e964dd03652
roworu <mamoruessu@gmail.com>
2026-09-03 17:13:00 +0300

    safe services disabling; add flathub

--- build_scripts/60-updates.sh
+++ build_scripts/60-updates.sh
@@ -2,14 +2,23 @@
 
 set -ouex pipefail
 
+curl -fsSL https://dl.flathub.org/repo/flathub.flatpakrepo -o /etc/flatpak/remotes.d/flathub.flatpakrepo
+
 ###
 ### disable auto-updates
 ###
 
-systemctl disable rpm-ostreed-automatic.timer
-systemctl disable flatpak-system-update.timer
+if systemctl list-unit-files rpm-ostreed-automatic.timer &>/dev/null; then
+	systemctl disable rpm-ostreed-automatic.timer
+fi
 
-systemctl --global disable flatpak-user-update.timer
+if systemctl list-unit-files flatpak-system-update.timer &>/dev/null; then
+	systemctl disable flatpak-system-update.timer
+fi
+
+if systemctl --global list-unit-files flatpak-user-update.timer &>/dev/null; then
+	systemctl --global disable flatpak-user-update.timer
+fi
 
 # kctl update checker from system_files/base/usr/lib/systemd/user/kctl-update-notify.timer
 systemctl --global enable kctl-update-notify.timer
