f59bf3fe1cfc31267de66bc75b7ba55aa4c84747
roworu <mamoruessu@gmail.com>
2026-09-03 21:16:45 +0300

    fix bad command

--- build_scripts/00-base.sh
+++ build_scripts/00-base.sh
@@ -88,7 +88,7 @@ dnf5 -y install \
 	"https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
 	"https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
-dnf5 -y intel-media-driver
+dnf5 -y install intel-media-driver
 dnf5 -y install mesa-va-drivers-freeworld
 dnf5 -y swap mesa-vulkan-drivers{,-freeworld}
 dnf5 -y install @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
