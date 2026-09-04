65e341226c0b7943416d0c610aad615a43fa7b32
roworu <mamoruessu@gmail.com>
2026-09-04 15:14:53 +0300

    Fix upload permissions; remove unneded fonts step

--- build_scripts/70-fonts.sh
+++ build_scripts/70-fonts.sh
@@ -5,5 +5,3 @@ set -ouex pipefail
 dnf5 -y install --nogpgcheck --repofrompath "terra,https://repos.fyralabs.com/terra\$releasever" terra-release
 dnf5 -y --setopt=terra.gpgcheck=0 install ubuntumono-nerd-fonts jetbrainsmono-nerd-fonts
 dnf5 -y remove terra-release terra-gpg-keys
-
-fc-cache -f
