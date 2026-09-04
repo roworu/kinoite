4a46f9551e75a9f5adc1b86fad8ae6db2d2f5a36
roworu <mamoruessu@gmail.com>
2026-09-04 14:18:34 +0300

    fix formatting

--- build_scripts/00-base.sh
+++ build_scripts/00-base.sh
@@ -85,10 +85,10 @@ fi
 ###
 
 dnf5 -y install \
-    "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+	"https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
 
 dnf5 -y install \
-    "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+	"https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
 dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
 dnf5 -y install intel-media-driver
